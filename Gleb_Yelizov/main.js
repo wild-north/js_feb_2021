42740a14463ed6267f37d72206e616a3eca59ac0
GlebYelizov <mrgleb91@gmail.com>
2021-03-17 19:59:39 +0200

    homework lesson_04

--- Gleb_Yelizov/main.js
+++ Gleb_Yelizov/main.js
@@ -27,3 +27,72 @@ function render() {
     main.append(ul);
     document.body.append(main);
 }
+//-------------------------------------------------------------------lesson_04 Homework------------------------------------------------
+
+//----------------get Paired array-----------------
+
+function getPaired(arr) {
+    let paired = [];
+    for (let i = 0; i <= arr.length; i++) {
+        if ((arr[i] % 2 == 0) && !(arr[i] == 0)) {
+        paired.push(arr[i]);
+        };
+    };
+    return paired;
+};
+
+//----------------get unPaired array-----------------
+
+function getUnPaired(arr) {
+    let unPaired = [];
+    for (let j = (arr.length - 1); j >= 0; j--) {
+        if (arr[j] !== 0) {
+            if (arr[j] % 2 !== 0) {
+                unPaired.push(arr[j]);
+            }
+        else continue;
+        };
+    };
+    return unPaired;
+};
+
+//----------------get indexes of found numbers-----------------
+
+function findIndex(array, elementToFind) {
+    let feetNumbers = [];
+    for (let i = 0; i < array.length; i++) {
+            if (array[i] == elementToFind) {
+                feetNumbers.push(i);
+            };
+                if (i == (array.length -1)) {
+                    if (feetNumbers.length > 0) {
+                        break
+                    }
+                    else {
+                        feetNumbers.push(-1);
+                    };
+                };
+    };
+    return feetNumbers;
+};
+
+//-------------------conditions--------------------------------
+function createArray(size) {
+    return [...new Array(size)].map((v,k) => k);
+} 
+
+const arr = createArray(100);
+let array = [1, 2, 3, 8, 4, 5, 6, 7, 8, 9, 10];
+let elementToFind = 8;
+
+document.write(`Original_array:${arr} <br \/>`);
+document.write(`Array with paired numbers:${getPaired(arr)} <br \/>`);
+document.write(`Array with unpaired numbers:${getUnPaired(arr)} <br \/>`);
+document.write(`Indexes of found elements: ${findIndex(array, elementToFind)} <br \/>`);
+
+
+
+
+
+
+
