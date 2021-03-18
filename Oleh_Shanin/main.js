0514fe11f30a2a5a9e2e76aae3db311aba39a7bf
izzilowe <izzilowe7@gmail.com>
2021-03-18 15:44:37 +0300

    my awful homework

--- Oleh_Shanin/main.js
+++ Oleh_Shanin/main.js
@@ -1,14 +1,49 @@
-function fromBeginningToEndy(size) {
-    const arr = [];
-
-    for (let i = 0; i < 100; i++) {
-      
-        if (i % 2 == 0) continue;
-
-        arr[i] = i + 1;
-    }
-
-    return arr;
+function createArray(size) {
+    return [...new Array(size)].map((v,k) => k);
 }
 
-const arr = fromBeginningToEndy(100);
+const arr = createArray(100);
+ 
+function returnEven() {
+    let total = [];
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            continue;   
+         } 
+        total.push (arr[i]);
+
+    }
+    return total
+}
+
+
+
+
+function returnOdd() {
+    let total = [];
+    for (i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] % 2 !== 0) {
+            continue;
+        }
+        total.push (arr[i]);
+
+    }
+    return total
+}
+
+
+function findIndex(array,elementToFind) {
+    let lookIndex = [];
+    for (i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === elementToFind) {
+            lookIndex.push(i);
+        } else (arr[i] !== elementToFind) {
+            return -1
+        }
+    }
+
+}
+  
+    
+
+
