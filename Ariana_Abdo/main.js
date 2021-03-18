62cd023089acbd85d5521db90adf486ecd88587b
Ариана Абдо <arianaabdo@MacBook-Pro-Ariana.local>
2021-03-18 13:44:56 +0200

    added three tasks

--- Ariana_Abdo/main.js
+++ Ariana_Abdo/main.js
@@ -1,29 +1,42 @@
-document.addEventListener('DOMContentLoaded', render);
-
-const text = {
-    header: 'This is the file for your home tasks',
-    list: [
-        'Please, write your JavaScript code in <strong>main.js</strong>',
-        'Please, write your CSS code in <strong>styles.css</strong>',
-        'Please, write your HTML right here in <strong>index.html</strong>'
-    ]
-};
-
-function render() {
-    const main = document.createElement('main');
-    const h2 = document.createElement('h2');
-    h2.textContent = text.header;
-
-    const ul = text.list.reduce((ul, text) => {
-        const li = document.createElement('li');
-
-        li.innerHTML = text;
-        ul.append(li);
-
-        return ul;
-    }, document.createElement('ul'));
-
-    main.append(h2);
-    main.append(ul);
-    document.body.append(main);
+ЗАДАЧА №1
+function createArray(size) {
+    return [...new Array(size)].map((v,k) => k);
 }
+
+const arr = createArray(100);
+for (let i = 1; i < arr.length; i++){
+    
+    if ((arr[i] % 2) == 0 ){
+        console.log(arr[i]);
+    }
+}
+
+ЗАДАЧА №2
+function createArray(size) {
+    return [...new Array(size)].map((v,k) => k);
+}
+
+const arr = createArray(100);
+for (let i = 99; i < arr.length; i--){
+    
+    if ((arr[i] % 2) === 1 ){
+        console.log(arr[i]);
+    }
+}
+
+ЗАДАЧА №3
+function findIndex (array, elementToFind) {
+
+    for (i = 0; i < array.length; i++) {
+
+        if(array[i] == elementToFind) {
+        return i;
+        break;
+        }
+    }
+    return -1
+}
+let array = [ 12, 30, 39, 40, 50, 56];
+
+let result = findIndex(array, 40);
+console.log (result)
