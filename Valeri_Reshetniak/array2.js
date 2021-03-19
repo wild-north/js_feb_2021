39d5184c3e96a4c4407c75c252c67a60fcf73e0c
valeri3110 <reshetnyak.valeriya@gmail.com>
2021-03-19 20:12:12 +0200

    correct task

--- Valeri_Reshetniak/array2.js
+++ Valeri_Reshetniak/array2.js
@@ -1,16 +1,8 @@
 function findIndex(array, elementToFind){
-    let temp = 0;
     for(i = 0; i < array.length; i++){
-        if( i === elementToFind){
-            console.log(array.indexOf(i));
-            temp = null;
-            break;
-        }
-        else {
-            continue;
-        }
+        if( array[i] === elementToFind){
+            return(i);  
+        }    
     }
-    if( temp !== null){
-        console.log(-1)
-    }
+  return(-1);
 }
