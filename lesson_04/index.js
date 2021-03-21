10e56ee617cd1c026e0f3939ea64189fd0886aee
Valentyne-91 <79361962+Valentine-91@users.noreply.github.com>
2021-03-21 16:00:19 +0200

    lesson_04

--- lesson_04/index.js
+++ lesson_04/index.js
@@ -1,33 +1,88 @@
-function createArray(size) {
-    const arr = [];
+//Задание 1 lesson_04
+// чётные по возрастанию 1 вариант:
 
-    for (let i = 0; i < size; i++) {
-        arr[i] = i + 1;
+function createArray(size) {
+    return [...new Array(size)].map((v,k) => k);
     }
 
-    return arr;
+const arr = createArray(100);
+
+    for (var i = 0; i < 100; i += 2) {
+                 console. log(arr[i]);
+    }
+//Нечётные по убыванию 1 вариант: 
+
+function createArray(size) {
+    return [...new Array(size)].map((v,k) => k);
+    }
+
+const arr = createArray(100);
+
+        for (var i = arr.length - 1; i >= 0; i -= 2) {
+        console. log(arr[i]);
+        }
+
+
+//чётные по возрастанию 2 вариант:
+
+function createArray(size) {
+    return [...new Array(size)].map((v,k) => k);
+    }
+
+const arr = createArray(100);
+
+logForward(arr);
+
+        function logForward(arr) {
+                for (var i = 2; i < 100; i += 2) {
+                console. log(arr[i]);
+                }
+        }
+
+ //Нечётные по убыванию 2 вариант:
+
+function createArray(size) {
+    return [...new Array(size)].map((v,k) => k);
+    }
+
+const arr = createArray(100);
+
+logBackward(arr)
+
+function logBackward(arr) {
+        for (var i = arr.length - 1; i >= 0; i -= 2) {
+            console. log(arr[i]);
+        }
+}
+//еще один вариант:
+
+const arr = [...Array(100).keys()];
+logForward(arr);
+logBackward(arr);
+
+function logForward(arr) {
+        for (var i = 0; i < arr.length; i += 2) {
+         console. log(arr[i]);
+        }
 }
 
-const arr = createArray(10);
+function logBackward(arr) {
+        for (var i = arr.length - 1; i >= 0 ; i -= 2) {
+            console. log(arr[i]);
+        }
+}
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+//Задание 2 lesson_04
 
-// for (let i = arr.length - 1; i >= 0; i--) {
-//     console.log(arr[i]);
-// }
-
-
-// for (let i = 10; i > 0; i--) {
-//     console.log(i);
-// }
-
-// ДЗ
-// написать функцию
-
-function findIndex(arr, elementToFind) {
-    // magic
-
-    return // индекс найденного или -1
+function findElement(arr, element) {
+  let index = -1;
+  
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === element) {
+      index = i;
+      break;
+    }
+  }
+  
+  return index;
 }
