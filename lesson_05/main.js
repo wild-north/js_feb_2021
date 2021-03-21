e39a1ceeea9bd82228529a02e82ff58a9d02fca9
Valentyne-91 <79361962+Valentine-91@users.noreply.github.com>
2021-03-21 22:36:50 +0200

    homme_work_lesson_05

--- lesson_05/main.js
+++ lesson_05/main.js
@@ -1,95 +1,38 @@
-// Solutions for the home task lesson 04
+// homme work lesson_05
+// пе понятно почему не заработало, нужна помощь
+let primeArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 14, 15]
 
-function createArray(size) {
-    const arr = [];
+myRevers(primeArr);
 
-    for (let i = 0; i < size; i++) {
-        arr[i] = i + 1;
-    }
+function  myRevers(arr) {
+    let newArr=[];
 
-    return arr;
-}
-
-const arr = createArray(10);
-
-function getEvenItems(array) {
-    const resultsArray = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 0 && i !== 0) {
-            resultsArray.push(arr[i]);
+        for (var i = arr.length - 1; i >= 0 ; i--) {
+        newArr.push(arr[i]);
         }
-    }
 
-    return resultsArray;
+    return [... newArr]
 }
 
-function getOddItems(array) {
-    const resultsArray = [];
+primeArr=myReverse(primeArr)
 
-    for (let i = 0; i < arr.length; i++) {
-        const currentItem = arr[arr.length - 1 - i];
+console.log(primeArr);
 
-        if (currentItem % 2 !== 0 && i !== 0) {
-            resultsArray.push(arr[i]);
+//рабочий вариант
+
+let primeArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 14, 15]
+
+myRevers(primeArr);
+
+function  myRevers(arr) {
+    let newArr=[];
+
+        for (var i = arr.length - 1; i >= 0 ; i--) {
+        newArr.push(arr[i]);
         }
-    }
 
-    return resultsArray;
+    // arr = newArr;
+        for (var j = 0; j < newArr.length; j++) arr[j] = newArr[j];
 }
 
-function findIndex(arr, elementToFind) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === elementToFind) {
-            return i;
-        }
-    }
-
-    return -1;
-}
-/////////////////////////////////////////////
-// forEach
-arr.forEach(function(item, i) {
-    document.write(`<dl>item: ${item}</dl><dt>index: ${i}</dt><hr/>`);
-});
-
-/////////////////////////////////////////////
-// map
-const mappedArray = arr.map(function(item, i) {
-    return {
-        id: i,
-        value: item
-    }
-});
-
-
-
-console.log('mappedArray', mappedArray);
-/////////////////////////////////////////////////
-// reduce
-const sum = arr.reduce(function(acc, item) {
-    acc += item;
-
-    return acc; // never forget to return the accumulator!
-}, 0);
-
-console.log('sum', sum);
-
-const object = arr.reduce(function(acc, item, index) {
-    acc[`element${index}`] = item;
-
-    return acc;
-}, {});
-
-console.log('object', object);
-
-///////////////////////////////////////////////////////
-
-// split,join
-const string = 'Bacon ipsum dolor amet capicola tenderloin shankle beef pastrami ham chicken. Landjaeger beef ribs cow, brisket shoulder jerky salami shankle t-bone. Hamburger venison bacon kevin, kielbasa sausage pork belly meatball landjaeger salami pancetta andouille filet mignon.'
-
-const arrayFromString = string.split(' '); // we use a space as a separator here
-
-const colorsArray = ['каждый', 'охотник', 'желает', 'знать', 'где', 'сидит', 'фазан'];
-
-const stringFromArray = colorsArray.join('.'); // use a dot as a separator here (try to use nothing ans see what would applied then)
+console.log(primeArr);
