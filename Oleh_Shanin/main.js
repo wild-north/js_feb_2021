c587219a49b3841f5e94b93a5bc6907da461ed0c
izzilowe <izzilowe7@gmail.com>
2021-03-24 16:17:01 +0300

    homework part 6

--- Oleh_Shanin/main.js
+++ Oleh_Shanin/main.js
@@ -39,3 +39,54 @@ function findIndex(arr, elementToFind) {
     return -1;
 };
   
+
+
+
+let c = 0;
+
+function createMatrix(size) {
+    const resultArray = [];
+
+    for (let i = 0; i < size; i++) {
+        resultArray[i] = [];
+        for (let j = 0; j < size; j++) {
+            // resultArray[i].push(`${i}:${j}`);
+            resultArray[i][j] = c++;
+        }
+    }
+
+    return resultArray;
+}
+
+const myArray = createMatrix(10);
+// const myArray2 = createMatrix(100);
+
+console.table(myArray);
+
+
+
+const w = 10;
+const h = 10;
+
+function removalFromTheMattress(w, h){
+    const result = [];
+    let startCol = 0;
+    let endCol = w - 1;
+    let startRow = 0;
+    let endRow = h - 1;
+        for (let i = startCol; i <= endCol; i++) {
+            result.push[i];
+        }
+        for (let j = startRow; j <= endRow; j++) {
+            result.push[j];
+        }
+        for (let k = endCol; k >= startCol; k--) {
+            result.push[k];
+        }
+        for (let o = endrow; o >= startRow; o--) {
+            result.push[o]
+        }
+
+
+    return result;
+};
