25741a1a4e24f2df786a462d9f0da0976c523ae1
Valentyne-91 <79361962+Valentine-91@users.noreply.github.com>
2021-03-29 18:49:11 +0300

    home_work_lesson_07

--- lesson_07/main.js
+++ lesson_07/main.js
@@ -1,209 +1,209 @@
-function createArray(size) {
-    const arr = [];
-
-    for (let i = 0; i < size; i++) {
-        arr[i] = i + 1;
-    }
-
-    return arr;
-}
-
-let c = 0;
-
-function createMatrix(size) {
-    const resultArray = [];
-
-    for (let i = 0; i < size; i++) {
-        resultArray[i] = [];
-        for (let j = 0; j < size; j++) {
-            // resultArray[i].push(`${i}:${j}`);
-            resultArray[i][j] = c++;
-        }
-    }
-
-    return resultArray;
-}
-
-const myArray = createMatrix(10);
-
-// console.table(myArray);
-
-function getParimeter(matrix) {
-    const topArray = matrix[0].slice();
-    const bottomArray = matrix[matrix.length - 1].slice().reverse();
-    const leftArray = [];
-    const rightArray = [];
-    
-    for (let i = 1; i < matrix.length - 1; i++) {
-        rightArray.push(matrix[i][matrix[i].length - 1]);
-        leftArray.push(matrix[i][0]);
-    }
-
-    leftArray.reverse();
-
-    return topArray.concat(rightArray, bottomArray, leftArray);
-}
-
-const sayHi = (name, ...params) => {   
-    console.log('привет, ' + name);
-    
-    params.forEach(v => console.log(v));
-};
-
-const sayHi2 = (...params) => {   
-    // console.log(params);
-    params.forEach((v,i) => console.log(`параметр ${i}`, v));
-};
-
-// sayHi2(1,2,3,4,5,6,7,8);
-
-function testArguments() {
-    console.log(arguments);
-}
-// testArguments(1,2,3,4,5,6,7,8,9);
-
-// function test(a) {
-//     console.log(a === myFunction);
+// function createArray(size) {
+//     const arr = [];
+//
+//     for (let i = 0; i < size; i++) {
+//         arr[i] = i + 1;
+//     }
+//
+//     return arr;
 // }
-
-// function myFunction() {
-//     console.log('привет');
+//
+// let c = 0;
+//
+// function createMatrix(size) {
+//     const resultArray = [];
+//
+//     for (let i = 0; i < size; i++) {
+//         resultArray[i] = [];
+//         for (let j = 0; j < size; j++) {
+//             // resultArray[i].push(`${i}:${j}`);
+//             resultArray[i][j] = c++;
+//         }
+//     }
+//
+//     return resultArray;
 // }
-
-// const result = myFunction();
-
-// test(result);
-// либо
-// test(myFunction());
-
-// const a = 10;
-
-// function test(fn) {
-//     const a = 100;
-
-//     fn();
+//
+// const myArray = createMatrix(10);
+//
+// // console.table(myArray);
+//
+// function getParimeter(matrix) {
+//     const topArray = matrix[0].slice();
+//     const bottomArray = matrix[matrix.length - 1].slice().reverse();
+//     const leftArray = [];
+//     const rightArray = [];
+//
+//     for (let i = 1; i < matrix.length - 1; i++) {
+//         rightArray.push(matrix[i][matrix[i].length - 1]);
+//         leftArray.push(matrix[i][0]);
+//     }
+//
+//     leftArray.reverse();
+//
+//     return topArray.concat(rightArray, bottomArray, leftArray);
 // }
-
-// function foo() {
-//     console.log(a);
+//
+// const sayHi = (name, ...params) => {
+//     console.log('привет, ' + name);
+//
+//     params.forEach(v => console.log(v));
+// };
+//
+// const sayHi2 = (...params) => {
+//     // console.log(params);
+//     params.forEach((v,i) => console.log(`параметр ${i}`, v));
+// };
+//
+// // sayHi2(1,2,3,4,5,6,7,8);
+//
+// function testArguments() {
+//     console.log(arguments);
 // }
-
-// test(foo);
-//////////////////////////////////
-const returnedValue = (function () {
-    const a = 10;
-
-    function getA() {
-        console.log(a);
-    }
-
-    return getA;
-})();
-
-const returnedValue = (function () {
-    const a = 10;
-
-    function getA() {
-        console.log(a);
-    }
-
-    return getA;
-})();
-
-console.log(returnedValue);
-
-returnedValue();
-
-////////////////////////
-function mainFunction() {
-    const a = 10;
-
-    function getA() {
-        console.log(a);
-    }
-
-    return getA;
-}
-
-const returnedValue = mainFunction();
-
-console.log(returnedValue);
-
-returnedValue();
-//////////////////////////////////
-
-function makeCounter() {
-    let count = 0;
-  
-    function inner() {
-      return count++;
-    };
-  
-    return inner;
-  }
-  
-  let counter = makeCounter();
-  let counter2 = makeCounter();
-  counter();
-  counter(); // ?
-  counter2(); // ?
-
-  ///////////////////////////////////
-  sum(1)
-
-function sum(a) {
-    return a;
-}
-///////////////
-function sum(a) {
-    return function(b) {
-        return a + b;
-    }
-}
-
-sum(1)(2);
-///////////////
-// const result = sum(a);
-
-// result(2);
-/////////////////////////////
-function sum(a) {
-    return function(b) {
-        return function(c) {
-            return a + b + c;
-        }
-    }
-}
-
-sum(1)(2)(3);
-
-const sum2 = (a) => (b) => (c) => a + b + c;
-
-function sum2(a) {
-    return function(b) {
-        return function(c) {
-            return a + b + c;
-        }   
-    }
-}
-////////////////////////////////////////////////
-
-const army = [];
-
-let counter = 0;
-
-while(counter < 10) {
-    counter++;
-
-    function solder() {
-        console.log(counter);
-    }
-    
-    army.push(solder);
-}
-
-army[0]();
-army[5]();
+// // testArguments(1,2,3,4,5,6,7,8,9);
+//
+// // function test(a) {
+// //     console.log(a === myFunction);
+// // }
+//
+// // function myFunction() {
+// //     console.log('привет');
+// // }
+//
+// // const result = myFunction();
+//
+// // test(result);
+// // либо
+// // test(myFunction());
+//
+// // const a = 10;
+//
+// // function test(fn) {
+// //     const a = 100;
+//
+// //     fn();
+// // }
+//
+// // function foo() {
+// //     console.log(a);
+// // }
+//
+// // test(foo);
+// //////////////////////////////////
+// const returnedValue = (function () {
+//     const a = 10;
+//
+//     function getA() {
+//         console.log(a);
+//     }
+//
+//     return getA;
+// })();
+//
+// const returnedValue = (function () {
+//     const a = 10;
+//
+//     function getA() {
+//         console.log(a);
+//     }
+//
+//     return getA;
+// })();
+//
+// console.log(returnedValue);
+//
+// returnedValue();
+//
+// ////////////////////////
+// function mainFunction() {
+//     const a = 10;
+//
+//     function getA() {
+//         console.log(a);
+//     }
+//
+//     return getA;
+// }
+//
+// const returnedValue = mainFunction();
+//
+// console.log(returnedValue);
+//
+// returnedValue();
+// //////////////////////////////////
+//
+// function makeCounter() {
+//     let count = 0;
+//
+//     function inner() {
+//       return count++;
+//     };
+//
+//     return inner;
+//   }
+//
+//   let counter = makeCounter();
+//   let counter2 = makeCounter();
+//   counter();
+//   counter(); // ?
+//   counter2(); // ?
+//
+//   ///////////////////////////////////
+//   sum(1)
+//
+// function sum(a) {
+//     return a;
+// }
+// ///////////////
+// function sum(a) {
+//     return function(b) {
+//         return a + b;
+//     }
+// }
+//
+// sum(1)(2);
+// ///////////////
+// // const result = sum(a);
+//
+// // result(2);
+// /////////////////////////////
+// function sum(a) {
+//     return function(b) {
+//         return function(c) {
+//             return a + b + c;
+//         }
+//     }
+// }
+//
+// sum(1)(2)(3);
+//
+// const sum2 = (a) => (b) => (c) => a + b + c;
+//
+// function sum2(a) {
+//     return function(b) {
+//         return function(c) {
+//             return a + b + c;
+//         }
+//     }
+// }
+// ////////////////////////////////////////////////
+//
+// const army = [];
+//
+// let counter = 0;
+//
+// while(counter < 10) {
+//     counter++;
+//
+//     function solder() {
+//         console.log(counter);
+//     }
+//
+//     army.push(solder);
+// }
+//
+// army[0]();
+//army[5]();
 // ДЗ - сделать так, чтобы вызовы функций возвращали "правильный" counter.
 
 // const a = 10;
@@ -211,3 +211,37 @@ army[5]();
 // (function(a) {
 //     console.log(a);
 // })(100);
+
+// //Черновик
+// let army = []
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+// }
+//
+// army[0]();
+
+// Home work lesson_07
+
+//1 вариант
+
+let army = [];
+
+for (let countA = 0; countA < 100; countA++) {
+    let countB = countA;
+    army.push(function () {
+        console.log(countB);
+    });
+}
+
+console.log(army);
+
+//2 вариант
+
+let army = [];
+
+for (let countA = 0; countA < 100; countA++) 
+    
+army. push (() => console.log (countA))
+
+
+console.log(army);
