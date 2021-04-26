c146295c0ba05d43baf6c8ba09d2f0cf7d57fe93
GlebYelizov <mrgleb91@gmail.com>
2021-04-26 20:14:57 +0300

    Homework Lesson_14 first commit

--- Gleb_Yelizov/main.js
+++ Gleb_Yelizov/main.js
@@ -144,3 +144,85 @@ tr1 = setTimeout( () => horse1.run(10), 5000);
 tr2 = setTimeout( () => horse2.run(10), 5000);
 tr3 = setTimeout( () => consLog(), 15000);
 
+//----------------------------------------Homework Lesson_13 After lesson_14-----------------------
+
+class MakeHorse1 {
+    constructor(name = 'Anonimous', initMileage = 0, initTiredness = 0) {
+        this.name = name;
+        this.mileage = initMileage;
+        this.tiredness = initTiredness;
+    }
+    
+    rest(myFunction, restToRun) {
+        console.log(`${this.name} is resting`);   
+        setTimeout( () => {
+            console.log(`${this.name} is running now`);
+            return myFunction.call(this, restToRun);
+        }, 5000);
+    }
+
+    run(miles) {
+        if (miles === 0) {
+            return console.log(`${this.name} has finished`);
+        } else if (this.tiredness === 10) {
+            this.tiredness = 0;      
+            return this.rest(this.run, miles);
+        }        
+        this.mileage += 1;
+        this.__proto__.totalMileage += 1;
+        this.tiredness += 1;        
+        this.run(--miles);
+    }
+}
+
+MakeHorse1.prototype.totalMileage = 0;
+
+const h1 = new MakeHorse1();
+
+//-------------------------------------------------Homework Lesson_14------------------------------
+
+class Clock {
+    getTime() {
+        let time = new Date();
+        
+        return console.log(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`);
+    }
+    
+    setAlarm(time) {        
+        let temp = new Date().setHours(...time.split(':'), '00') - new Date().getTime();
+
+        if (temp > 300000 || temp < 0) {
+            return console.log('не более 5 минут и в будущем');
+        }
+        
+        setTimeout( () => console.log('Alarm'), temp);        
+    }
+
+    setTimer(x) {
+        let temp = () => ( +(x.split(':')[0] * 60) + +(x.split(':')[1]) );
+        
+        
+        this.timer(temp());
+        
+    }
+
+    timer(y) {
+        if (y === 0) {
+            return console.log('Alarm');           
+        }
+        setTimeout ( () => {
+            console.log(`${y}`);
+            return this.timer(--y);        
+        }, 1000)
+    }
+}
+
+let clock = new Clock();
+
+clock.getTime(); // выводит время в консоль (в формате 21:36:02)
+
+clock.setAlarm('18:26');
+
+clock.setTimer('0:15'); // в консоли выводить обратный отсчет до 0
+
+
