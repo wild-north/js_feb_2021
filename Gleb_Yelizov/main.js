d2fecd2b78ea156a9d7198c943677cae926862b1
GlebYelizov <mrgleb91@gmail.com>
2021-05-14 18:57:08 +0300

    Homework lesson_16_17

--- Gleb_Yelizov/main.js
+++ Gleb_Yelizov/main.js
@@ -1,23 +1,35 @@
 
 
-//Left Menu (Solution based on CSS)-----------------------------------------------------------------------------
+// ---------------Lesson_16 Homework----------------------------------------------------------------------------
+let docBody = document.getElementsByTagName('body')[0];
+
+//Left Menu (Solution based on CSS)---------------------------------------
 
 let menuButtonLeft = document.querySelector('.toggler');
 let menuLeft = document.querySelector('.menu');
 
-// menuButtonLeft.addEventListener("click", showMenu);
+menuButtonLeft.addEventListener("click", showMenu);
 
-// function showMenu() {
-//     menuLeft.classList.toggle('active');
-// }
+function showMenu(event) {
+    event.stopPropagation();
+    menuLeft.classList.toggle('active');
+    if (menuLeft.classList.contains('active') ) {
+        return docBody.addEventListener("click", hideMenu);        
+    }
+    docBody.removeEventListener("click", hideMenu);
+}
+
+function hideMenu() {
+    menuLeft.classList.remove('active');
+}
 
 //----Variation with arrow function------
 
-menuButtonLeft.addEventListener("click", () => menuLeft.classList.toggle('active'));
+// menuButtonLeft.addEventListener("click", () => menuLeft.classList.toggle('active'));
 
 
 
-//Right Menu (Solutions based on JS)----------------------------------------------------------------------------------
+//Right Menu (Solutions based on JS)---------------------------------------
 
 let menuButtonRight = document.querySelector('.toggle');
 let menuRight = document.querySelector('._menu');
@@ -33,6 +45,7 @@ let menuRight = document.querySelector('._menu');
 //----Variation with function declaration------
 
 menuButtonRight.addEventListener("click", menuShow);
+//docBody.addEventListener("click", hideMenu);
 
 function menuShow() {
     let temp = menuRight.getAttribute('style');
@@ -54,7 +67,7 @@ function menuShow() {
 //     }
 // }
 
-//-----Additional practice------------------------------------------------------------------------
+//-----Additional practice----------------------------------------------
 
 let reloadButton = document.querySelector('#reload_button');
 let liElements = document.querySelectorAll('ul > li');
@@ -70,9 +83,35 @@ function doReload() {
 }
 
 
+// ---------------Lesson_17 Homework-----------------------------------------------------------------------------------
 
 
+let field = document.querySelector('#field');
+let ball = document.querySelector('#ball');
 
+field.addEventListener('click', moveBall);
+
+function moveBall(event) {
+    let fieldCoords = field.getBoundingClientRect();
+    let ballTop = event.clientY - fieldCoords.top - field.clientTop - ball.offsetHeight / 2;
+    let ballLeft = event.clientX - fieldCoords.left - field.clientLeft - ball.offsetWidth / 2;
+    
+    if (ballTop < 0) {
+        ballTop = 0;
+    }
+    if (ballLeft < 0) {
+        ballLeft = 0;
+    }
+    if (ballTop > field.clientHeight - ball.offsetHeight) {
+        ballTop = field.clientHeight - ball.offsetHeight;
+    }
+    if (ballLeft > field.clientWidth - ball.offsetWidth) {
+        ballLeft = field.clientWidth - ball.offsetWidth;
+    }
+
+    ball.style.top = ballTop + 'px';
+    ball.style.left = ballLeft +'px';    
+}
 
 
 
