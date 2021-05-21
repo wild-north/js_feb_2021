4ed39e12183ce8535fb34601848aa67609444170
GlebYelizov <mrgleb91@gmail.com>
2021-05-21 17:10:22 +0300

    Homework lesson_20

--- Gleb_Yelizov/main.js
+++ Gleb_Yelizov/main.js
@@ -1,166 +1,255 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
+//---------------------------------------------------Lesson 19----------------------------------------- 
 
-// ---------------Lesson_16 Homework----------------------------------------------------------------------------
-let docBody = document.getElementsByTagName('body')[0];
+    let xhrOutput = [];
+    let awaitOutput = [];
+    let promiseOutput = [];
 
-//Left Menu (Solution based on CSS)---------------------------------------
+    setTimeout(() => xhrResponse('https://jsonplaceholder.typicode.com/users'), 20);
+    setTimeout(() => awaitResponse('https://jsonplaceholder.typicode.com/users'), 40);
+    setTimeout(() => promiseResponse('https://jsonplaceholder.typicode.com/users'), 60);
+        
+    //------------------------xhr-------------------------------
 
-let menuButtonLeft = document.querySelector('.toggler');
-let menuLeft = document.querySelector('.menu');
-
-menuButtonLeft.addEventListener("click", showMenu);
-
-function showMenu(event) {
-    event.stopPropagation();
-    menuLeft.classList.toggle('active');
-    if (menuLeft.classList.contains('active')) {
-        return docBody.addEventListener("click", hideMenu);
-    }
-    docBody.removeEventListener("click", hideMenu);
-}
-
-function hideMenu(event) {
-    if (!event.target.closest('.menu')) {
-        menuLeft.classList.remove('active');
-    }
-
-}
-
-//----Variation with arrow function------
-
-// menuButtonLeft.addEventListener("click", () => menuLeft.classList.toggle('active'));
-
-
-
-//Right Menu (Solutions based on JS)---------------------------------------
-
-let menuButtonRight = document.querySelector('.toggle');
-let menuRight = document.querySelector('._menu');
-
-//----First solution--------------------------------------------------
-
-// menuButtonRight.addEventListener("click", () => {
-//     menuRight.getAttribute('style') === "display: block"
-//     ? menuRight.setAttribute('style', "display: none")
-//     : menuRight.setAttribute('style', "display: block")
-// });
-
-//----Variation with function declaration------
-
-menuButtonRight.addEventListener("click", menuShow);
-//docBody.addEventListener("click", hideMenu);
-
-function menuShow() {
-    let temp = menuRight.getAttribute('style');
-
-    let temp2 = (temp === "display: block") ? 
-    menuRight.setAttribute('style', "display: none") : menuRight.setAttribute('style', "display: block");
-}
-
-//----Second solution---------------------------------------------------
-
-// menuButtonRight.onclick = menuShow;
-
-// function menuShow() {
-//     if (menuRight.style.display === 'block') {
-//         menuRight.style.display = 'none';
-//     } else {
-//         menuRight.style.display = 'block';
-//     }
-// }
-
-//-----Additional practice----------------------------------------------
-
-let reloadButton = document.querySelector('#reload_button');
-let liElements = document.querySelectorAll('ul > li');
-
-reloadButton.addEventListener('click', doReload, false);
-
-for (let i = 0; i < liElements.length; i++) {
-    liElements[i].addEventListener('click', doReload);
-}
-
-function doReload() {
-    location.reload();
-}
-
-
-// ---------------Lesson_17 Homework-----------------------------------------------------------------------------------
-
-
-let field = document.querySelector('#field');
-let ball = document.querySelector('#ball');
-
-field.addEventListener('click', moveBall);
-
-function moveBall(event) {
-    let fieldCoords = field.getBoundingClientRect();
-    let ballTop = event.clientY - fieldCoords.top - field.clientTop - ball.offsetHeight / 2;
-    let ballLeft = event.clientX - fieldCoords.left - field.clientLeft - ball.offsetWidth / 2;
-
-    if (ballTop < 0) {
-        ballTop = 0;
-    }
-    if (ballLeft < 0) {
-        ballLeft = 0;
-    }
-    if (ballTop > field.clientHeight - ball.offsetHeight) {
-        ballTop = field.clientHeight - ball.offsetHeight;
-    }
-    if (ballLeft > field.clientWidth - ball.offsetWidth) {
-        ballLeft = field.clientWidth - ball.offsetWidth;
+    function xhrResponse(url) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.responseType = 'json';
+        xhr.send();
+        xhr.onload = function () {
+            if (xhr.status != 200) {
+                console.log('Ошибка: ' + xhr.status);
+                return;
+            } else {
+                try {
+                    if (!Array.isArray(xhr.response)) {
+                        throw new Error("Данные приняты не в виде массива");
+                    }
+                    responseToArr(xhr.response, xhrOutput);
+                } catch (error) {
+                    if (error.name == "Error") {
+                        alert("JSON Error: " + error.message);
+                    } else {
+                        throw error;
+                    }
+                }
+            }
+            appendUl(xhrOutput, 'users_list');
+            beforeH3('.users_list', 'List of users made by xhr:');
+        };
+        xhr.onprogress = function (event) {
+            if (event.lengthComputable) {
+                console.log(`Получено ${event.loaded} из ${event.total} байт`);
+            } else {
+                console.log(`Получено ${event.loaded} байт`);
+            }
+        };
+        xhr.onerror = function () {
+            console.log('error');
+        };
     }
 
-    ball.style.top = ballTop + 'px';
-    ball.style.left = ballLeft + 'px';
-}
+    //-----------------------------await----------------------------------    
+
+    async function awaitResponse(url) {
+        let awaitResponse = await fetch(url);
+        if (awaitResponse.ok) {
+            let json = await awaitResponse.json();
+            //json = JSON.stringify(json);   --> для проверки
+            if (!Array.isArray(json)) {
+                throw new Error("Данные приняты не в виде массива");
+            }
+            responseToArr(json, awaitOutput);
+            appendUl(awaitOutput, 'users_list_2');
+            beforeH3('.users_list_2', 'List of users made by await:');
+        } else {
+            alert("Ошибка HTTP: " + awaitResponse.status);
+        }
+    }
+
+    //-----------------------------promis---------------------------------- 
+
+    function promiseResponse(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(users => responseToArr(users, promiseOutput))
+            .then(() => appendUl(promiseOutput, 'users_list_3'))
+            .then(() => beforeH3('.users_list_3', 'List of users made by promise:'))
+            .catch(err => alert(err));
+    }
+
+    //----------------------------------Functions----------------------------
+
+    function responseToArr(response, toArray) {
+        response.map((item) => {
+            toArray.push('User (id #' + item.id + ') - ' + item.name);
+        });
+    }
+
+
+    function appendUl(result, ulName) {
+        let ul = document.createElement('ul');
+
+        ul.append(appendLi(result));
+
+        document.body.append(ul);
+        ul.className = ulName;
+        ul.style.listStyle = 'none';
+    }
+
+    function appendLi(result) {
+        let fragment = new DocumentFragment();
+
+        for (let i = 0; i < result.length; i++) {
+            let li = document.createElement('li');
+            li.append(result[i]);
+            fragment.append(li);
+        }
+
+        return fragment;
+    }
+
+    function beforeH3(elem, text) {
+        let header = document.createElement('h3');
+        document.querySelector(elem).before(header);
+        header.innerHTML = text;
+    }
+
+
+    //---------------------------------------------------Lesson 20-----------------------------------------    
+
+
+    class Blank {
+        constructor(name, url) {
+            this.name = name;
+            this.url = url;
+        }
+
+        getAndBuild() {
+            let section = document.createElement('section');
+            section.id = `${this.name}`;
+
+            /* let sectionHeader = document.createElement('h3');            
+            sectionHeader.innerHTML = `Section: ${this.name}`;
+            section.before(sectionHeader); */
+
+            fetch(this.url)
+                .then(response => response.json())
+                .then(item => {
+                    item.map((user) => {                        
+                        section.append(this.build(user));
+                        section.style.cssText = `display: flex; flex-wrap: wrap;
+                            border: 1px black solid; margin: 10px;`;                        
+                    });
+                })
+                .then(document.body.append(section))
+                .catch(err => alert(err));
+        }
+        build(item) {
+            let userUl = document.createElement('ul');
+
+            for (let prop in item) {
+                let userLi = document.createElement('li');
+                if (typeof (item[prop]) !== 'object') {
+                    userLi.innerHTML = `${prop}: ${item[prop]}`;
+                    userUl.append(userLi);
+                } else {
+                    userLi.innerHTML = `${prop}: `;
+                    userLi.append(this.build(item[prop]));
+                    userUl.append(userLi);
+                }
+            }
+            userUl.style.cssText = `min-width: 185px; max-width: 16%`;
+            return userUl;
+        }
+    }
+
+    let firstSection = new Blank('users1', 'https://jsonplaceholder.typicode.com/users');
+
+    firstSection.getAndBuild();
+
+    let secondSection = new Blank('users2', 'https://jsonplaceholder.typicode.com/users');
+
+    secondSection.getAndBuild();
 
 
 });
 
 
 
+//----------------------------------------------------------------------------------------------
 
+/* showUsers('https://jsonplaceholder.typicode.com/users');
+    //debugger;
+    function showUsers(url) {
+        let sectionUsersData = document.createElement('section');
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
-xhr.responseType = 'json';
-xhr.send();
-
-xhr.onload = function() {
-    if (xhr.status != 200) {
-        console.log('Ошибка: ' + xhr.status);
-        return;
-    } else {
-        let resultToappend = [];
-        let ul = document.createElement('ul');
-    
-        xhr.response.map((item, index) => {
-            resultToappend.push(item.id + ' - ' + item.name)});
-        
+        fetch(url)
+            .then(response => response.json())
+            .then(item => {
+                item.map((user) => {
+                    sectionUsersData.append(appendIt(user));
+                });
+            })
+            .then(document.body.append(sectionUsersData))
+            .catch(err => alert(err));
     }
 
-xhr.onerror = function() {
-    console.log('error');    
-};
+    function appendIt(item) {
+        let userUl = document.createElement('ul');
+
+        for (let prop in item) {
+            let userLi = document.createElement('li');
+            if (typeof (item[prop]) !== 'object') {
+                userLi.innerHTML = `${prop}: ${item[prop]}`;
+
+                userUl.append(userLi);
+            } else {
+                userLi.innerHTML = `${prop}: `;
+                userLi.append(appendIt(item[prop]));
+                userUl.append(userLi);
+            }
+        }
+        return userUl;
+    } */
 
 
-function appendLi(result) {
-  let fragment = new DocumentFragment();
+    /* let promiseObj = {
+        url: 'https://jsonplaceholder.typicode.com/users',
 
-  for (let i = 0; i < result.length; i++) {
-    let li = document.createElement('li');
-    li.append();
-    fragment.append(li);
-  }
+        getAndBuild(url) {
+            let section = document.createElement('section');
+            section.id = `${this}`;
 
-  return fragment;
-}
+            fetch(url)
+                .then(response => response.json())
+                .then(item => {
+                    item.map((user) => {
+                        section.append(this.build(user));
+                    });
+                })
+                .then(document.body.append(section))
+                .catch(err => alert(err));
+        },
 
-ul.append(getListContent()); // (*)
-    
-};
+        build(item) {
+            let userUl = document.createElement('ul');
 
+            for (let prop in item) {
+                let userLi = document.createElement('li');
+                if (typeof (item[prop]) !== 'object') {
+                    userLi.innerHTML = `${prop}: ${item[prop]}`;
 
+                    userUl.append(userLi);
+                } else {
+                    userLi.innerHTML = `${prop}: `;
+                    userLi.append(this.build(item[prop]));
+                    userUl.append(userLi);
+                }
+            }
+            return userUl;
+        }
+    };
 
+    promiseObj.getAndBuild(promiseObj.url); */
