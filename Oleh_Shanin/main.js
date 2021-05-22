45fe2301652df492b9dc99babbe787e9f318e6f6
izzilowe <izzilowe7@gmail.com>
2021-05-22 20:49:43 +0300

    my home work part 19 - 20

--- Oleh_Shanin/main.js
+++ Oleh_Shanin/main.js
@@ -1,29 +1,78 @@
-document.addEventListener('DOMContentLoaded', render);
+const xhr = new XMLHttpRequest();
 
-const text = {
-    header: 'This is the file for your home tasks',
-    list: [
-        'Please, write your JavaScript code in <strong>main.js</strong>',
-        'Please, write your CSS code in <strong>styles.css</strong>',
-        'Please, write your HTML right here in <strong>index.html</strong>'
-    ]
+xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+
+xhr.send();
+
+xhr.onload = function () {
+  const data = JSON.parse(xhr.responseText);
+
+  for (let i = 0; i < data.length; i++) {
+    //    console.log(data[i].username);
+    let li = document.createElement("li");
+    li.innerHTML = data[i].username;
+    ul.appendChild(li);
+    document.body.append(ul);
+  }
 };
 
-function render() {
-    const main = document.createElement('main');
-    const h2 = document.createElement('h2');
-    h2.textContent = text.header;
+let ul = document.createElement("ul");
 
-    const ul = text.list.reduce((ul, text) => {
-        const li = document.createElement('li');
+//...... FETCH
 
-        li.innerHTML = text;
-        ul.append(li);
+let url = "https://jsonplaceholder.typicode.com/users";
 
-        return ul;
-    }, document.createElement('ul'));
+const makeCall = () =>
+  new Promise(function (resolve, reject) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => renderData(data))
+      .catch((err) => console.warn(err));
+  });
 
-    main.append(h2);
-    main.append(ul);
-    document.body.append(main);
+function renderData(data) {
+  let ul = document.createElement("ul");
+
+  for (let i = 0; i < data.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = data[i].username;
+    ul.appendChild(li);
+  }
+  document.body.append(ul);
 }
+
+makeCall();
+//.... FETCH 2
+
+const makeCall = () => fetch(url);
+
+makeCall()
+  .then((response) => response.json())
+  .then((data) => renderData(data))
+  .catch((err) => console.warn(err));
+
+function renderData(data) {
+  let ul = document.createElement("ul");
+
+  for (let i = 0; i < data.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = data[i].username;
+    ul.appendChild(li);
+  }
+  document.body.append(ul);
+}
+
+//.... AWAIT
+
+async function makeCallAwait() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    renderData(data);
+  } catch (error) {
+    console.error(error);
+  } finally {
+  }
+}
+
+makeCallAwait();
